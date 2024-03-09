bfbff6d74cdf0ec8a91d2d758234c37f526d2819
michelcwn <michel.cwn@gmail.com>
2024-03-09 13:20:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,300 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+const collisionsMap = [];
+for (let i = 0; i < collisions.length; i += 70) {
+  collisionsMap.push(collisions.slice(i, 70 + i));
+}
+
+const battleZonesMap = [];
+for (let i = 0; i < battleZonesData.length; i += 70) {
+  battleZonesMap.push(battleZonesData.slice(i, 70 + i));
+}
+
+const boundaries = [];
+const offset = {
+  x: -740,
+  y: -650,
+};
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      // 1025 = collision dans le JSON
+      boundaries.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        })
+      );
+  });
+});
+
+const battleZones = [];
+battleZonesMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      // 1025 = collision dans le JSON
+      battleZones.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        })
+      );
+  });
+});
+
+const image = new Image();
+image.src = "./img/Vera-Town.png";
+
+const foregroundImage = new Image();
+foregroundImage.src = "./img/foregroundObjects.png";
+
+const playerDownImage = new Image();
+playerDownImage.src = "./img/playerDown.png";
+const playerUpImage = new Image();
+playerUpImage.src = "./img/playerUp.png";
+const playerLeftImage = new Image();
+playerLeftImage.src = "./img/playerLeft.png";
+const playerRightImage = new Image();
+playerRightImage.src = "./img/playerRight.png";
+
+const player = new Sprite({
+  position: {
+    x: canvas.width / 2 - 192 / 4 / 2, // largeur image original
+    y: canvas.height / 2 - 68 / 2, // hauteur image original
+  },
+  image: playerDownImage,
+  frames: {
+    max: 4,
+  },
+  sprites: {
+    up: playerUpImage,
+    left: playerLeftImage,
+    right: playerRightImage,
+    down: playerDownImage,
+  },
+});
+
+const background = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: image,
+});
+
+const foreground = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: foregroundImage,
+});
+
+const keys = {
+  z: {
+    pressed: false,
+  },
+  q: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+const movables = [background, ...boundaries, foreground, ...battleZones];
+
+const rectangularCollision = function ({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+    rectangle1.position.y + 35 <= rectangle2.position.y + rectangle2.height && //+35 réduit la hauteur du personnage
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+  );
+};
+
+const animate = function () {
+  window.requestAnimationFrame(animate);
+  background.draw();
+  boundaries.forEach((boundary) => {
+    boundary.draw();
+  });
+  battleZones.forEach((battleZone) => {
+    battleZone.draw();
+  });
+  player.draw();
+  foreground.draw();
+
+  // Définir la source de playerImage après avoir établi le gestionnaire onload
+
+  // boundaries du player
+  let moving = true;
+  player.moving = false;
+
+  if (keys.z.pressed && lastKey === "z") {
+    player.moving = true;
+    player.image = player.sprites.up;
+
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x,
+              y: boundary.position.y + 3,
+            },
+          },
+        })
+      ) {
+        console.log("colliding");
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.y += 3;
+      });
+
+    // q
+  } else if (keys.q.pressed && lastKey === "q") {
+    player.moving = true;
+    player.image = player.sprites.left;
+
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x + 3,
+              y: boundary.position.y,
+            },
+          },
+        })
+      ) {
+        console.log("colliding");
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.x += 3;
+      });
+
+    //s
+  } else if (keys.s.pressed && lastKey === "s") {
+    player.moving = true;
+    player.image = player.sprites.down;
+
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x,
+              y: boundary.position.y - 3,
+            },
+          },
+        })
+      ) {
+        console.log("colliding");
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.y -= 3;
+      });
+
+    //d
+  } else if (keys.d.pressed && lastKey === "d") {
+    player.moving = true;
+    player.image = player.sprites.right;
+
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x - 3,
+              y: boundary.position.y,
+            },
+          },
+        })
+      ) {
+        console.log("colliding");
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.x -= 3;
+      });
+  }
+};
+animate();
+
+let lastKey = "";
+window.addEventListener("keydown", function (e) {
+  switch (e.key) {
+    case "z":
+      keys.z.pressed = true;
+      lastKey = "z";
+      break;
+    case "q":
+      keys.q.pressed = true;
+      lastKey = "q";
+      break;
+    case "s":
+      keys.s.pressed = true;
+      lastKey = "s";
+      break;
+    case "d":
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
+  }
+});
+
+window.addEventListener("keyup", function (e) {
+  switch (e.key) {
+    case "z":
+      keys.z.pressed = false;
+      break;
+    case "q":
+      keys.q.pressed = false;
+      break;
+    case "s":
+      keys.s.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+  }
+});
